67bc6906f161594d77a5c0ad7c099e70accb5fbf
PC-KANTOOR\René <rsh.kwee@gmail.com>
2023-02-28 17:17:17 +0100

    cmd file introduced.

--- HelpChm.cmd
+++ HelpChm.cmd
@@ -1,3 +1,5 @@
-
-"C:\Program Files (x86)\HTML Help Workshop\hhc" %1% > log.txt
+REM Batch file for genberating a CHM file.
+REM The hhc.exe returns an exit code 1, when the command is successfull....
+REM This batch file superseeds the exit code to 0 
+"C:\Program Files (x86)\HTML Help Workshop\hhc" %1%
 exit 0
