0d4167bfbc2b59d8d4fabc7e85635e56f6db728b
PCPrive2017\René <rsh.kwee@gmail.com>
2023-03-01 10:04:38 +0100

    Order change...

--- HelpChm.cmd
+++ HelpChm.cmd
@@ -1,4 +1,4 @@
-REM Batch file for genberating a CHM file.
+REM Batch file for generating a CHM file.
 REM The hhc.exe returns an exit code 1, when the command is successfull....
 REM This batch file superseeds the exit code to 0 
 "C:\Program Files (x86)\HTML Help Workshop\hhc" %1%
