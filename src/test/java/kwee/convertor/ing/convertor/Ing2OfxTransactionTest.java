fe3b9961d78a4d9d6fdcf8cd7bd2bf112b4815d4
PCPrive2017\René <rsh.kwee@gmail.com>
2023-06-13 15:35:35 +0200

    Begin maken met Junit 5 testcases.

--- src/test/java/kwee/convertor/ing/convertor/Ing2OfxTransactionTest.java
+++ src/test/java/kwee/convertor/ing/convertor/Ing2OfxTransactionTest.java
@@ -1,8 +1,14 @@
 package kwee.convertor.ing.convertor;
 
+import java.io.File;
+import java.net.URL;
+
 import junit.framework.TestCase;
 
 public class Ing2OfxTransactionTest extends TestCase {
+  private String c_IngTransFile = "Alle_rekeningen.csv";
+  private String c_IngSavingTransFile = "Alle_spaarrekeningen.csv";
+  private String c_SynonymFile = "Synoniem.txt";
 
   @Override
   public void setUp() {
@@ -14,10 +20,52 @@ public class Ing2OfxTransactionTest extends TestCase {
 
   public void testConvertSavingToOfx() {
     fail("Not yet implemented");
+    File csvFile = null;
+    File synFile = null;
+    // Get the URL of csv file
+    URL resourceUrl = getClass().getClassLoader().getResource(c_IngSavingTransFile);
+    if (resourceUrl != null) {
+      // Get the resource directory path
+      String resourceDirectory = resourceUrl.getPath();
+      csvFile = new File(resourceDirectory);
+    }
+
+    // Get the URL of synonym file
+    resourceUrl = getClass().getClassLoader().getResource(c_SynonymFile);
+    if (resourceUrl != null) {
+      // Get the resource directory path
+      String resourceDirectory = resourceUrl.getPath();
+      synFile = new File(resourceDirectory);
+    }
+
+    IngTransactions l_trans = new IngTransactions(csvFile, synFile);
+    l_trans.getIngSavingTransactions();
+
   }
 
   public void testConvertToOfx() {
     fail("Not yet implemented");
+    File csvFile = null;
+    File synFile = null;
+    // Get the URL of csv file
+    URL resourceUrl = getClass().getClassLoader().getResource(c_IngTransFile);
+    if (resourceUrl != null) {
+      // Get the resource directory path
+      String resourceDirectory = resourceUrl.getPath();
+      csvFile = new File(resourceDirectory);
+    }
+
+    // Get the URL of synonym file
+    resourceUrl = getClass().getClassLoader().getResource(c_SynonymFile);
+    if (resourceUrl != null) {
+      // Get the resource directory path
+      String resourceDirectory = resourceUrl.getPath();
+      synFile = new File(resourceDirectory);
+    }
+
+    IngTransactions l_trans = new IngTransactions(csvFile, synFile);
+    l_trans.getIngTransactions();
+
   }
 
 }
