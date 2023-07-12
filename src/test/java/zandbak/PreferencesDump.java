e0731478d9b4056cd055317b23c52c6193f773bc
PCPrive2017\René <rsh.kwee@gmail.com>
2023-07-12 16:55:07 +0200

    Layout

--- src/test/java/zandbak/PreferencesDump.java
+++ src/test/java/zandbak/PreferencesDump.java
@@ -1,13 +1,17 @@
 package zandbak;
 
 import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.prefs.*;
 
 public class PreferencesDump {
+  private static final Logger LOGGER = Logger.getLogger(Class.class.getName());
+
   public static void main(String[] args) throws BackingStoreException {
     Preferences prefs = Preferences.userRoot(); // or Preferences.systemRoot() for system preferences
 
-    dumpPreferences("", prefs);
+    LOGGER.log(Level.INFO, dumpPreferences("", prefs));
 
     String filePath;
     try {
@@ -19,14 +23,16 @@ public class PreferencesDump {
     }
   }
 
-  private static void dumpPreferences(String nodeName, Preferences prefs) throws BackingStoreException {
+  private static String dumpPreferences(String nodeName, Preferences prefs) throws BackingStoreException {
+    String lstr = "";
     // Retrieve all preference keys
     String[] keys = prefs.keys();
 
     // Iterate through the keys and retrieve their values
     for (String key : keys) {
       String value = prefs.get(key, null);
-      System.out.println(nodeName + "/" + key + " = " + value);
+      lstr = lstr + nodeName + " | " + key + " = " + value + "\n ";
+      // System.out.println(nodeName + "/" + key + " = " + value);
     }
 
     // Retrieve all child nodes
@@ -35,9 +41,10 @@ public class PreferencesDump {
     // Recursively dump the child nodes
     for (String childNode : childNodes) {
       Preferences childPrefs = prefs.node(childNode);
-      String childNodeName = nodeName.isEmpty() ? childNode : nodeName + "/" + childNode;
-      dumpPreferences(childNodeName, childPrefs);
+      String childNodeName = nodeName.isEmpty() ? childNode : nodeName + " / " + childNode;
+      lstr = lstr + dumpPreferences(childNodeName, childPrefs) + "\n ";
     }
+    return lstr;
   }
 
   private static String getPreferencesFilePath(Preferences prefs) throws Exception {
@@ -45,11 +52,9 @@ public class PreferencesDump {
     prefsFileField.setAccessible(true);
 
     Object prefsFile = prefsFileField.get(prefs);
-
     if (prefsFile instanceof java.io.File) {
       return ((java.io.File) prefsFile).getAbsolutePath();
     }
-
     return null;
   }
 }
