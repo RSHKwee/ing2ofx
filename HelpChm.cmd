aa3dcf264c819eba944d748be2b31bc1de998ab0
René Kwee <rsh.kwee@gmail.com>
2025-02-19 16:04:12 +0100

    Tools are maintained one directory up, POM modified accordingly.

--- HelpChm.cmd
+++
@@ -1,5 +0,0 @@
-REM Batch file for generating a CHM file.
-REM The hhc.exe returns an exit code 1, when the command is successfull....
-REM This batch file superseeds the exit code to 0 
-"C:\Program Files (x86)\HTML Help Workshop\hhc" %1%
-exit 0
--- innosetup.cmd
+++
@@ -1,5 +0,0 @@
-REM Batch file for InnoSetup.
-REM The hhc.exe returns an exit code 1, when the command is successfull....
-REM This batch file superseeds the exit code to 0 
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" %1%
-exit 0
